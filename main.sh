9bd754e6fcc3d97d91ad353c2a83c3cafb469f1b
Luigi <luigiiazzetta98@gmail.com>
2023-12-20 14:39:46 +0100

    Added the fucntion to connect automatically with a country code

--- main.sh
+++ main.sh
@@ -11,24 +11,27 @@ read -p "Select country code (cyberghost --country-code to see all):" COUNTRY_CO
     if [[ -z $COUNTRY_CODE ]]; then
         error_country_code
     fi
-    cyberghostvpn --country-code $COUNTRY_CODE 2>/dev/null
+    cyberghostvpn --traffic --country-code $COUNTRY_CODE 2>/dev/null
 } || {
     error_country_code
 }
 
 echo ""
-read -p "Select a City from avaiable:" CITY
+read -p "Select a City from avaiable (leave blank for automatic city):" CITY
 
-if [[ $(cyberghostvpn --country-code $COUNTRY_CODE --city $CITY) && -n $CITY ]]; then
-    SCRAPED_VALUES=$(cyberghostvpn --country-code $COUNTRY_CODE --city $CITY | sed '$ d' | sed '1,3d' | tr -d '|' | awk '{print $3,$4}' | sort -t' ' -k2 -n)
-    
-    SERVER=$(echo $SCRAPED_VALUES | awk '{print $1}')
-   
+if [[ -z $CITY ]]; then
+    sudo cyberghostvpn --traffic --country-code $COUNTRY_CODE --connect
 else
-    echo "Error, no City avaiable for the country code selected"
-    exit 1
+    if [[ $(cyberghostvpn --traffic --country-code $COUNTRY_CODE --city $CITY) && -n "$CITY" ]]; then
+        SCRAPED_VALUES=$(cyberghostvpn --traffic --country-code $COUNTRY_CODE --city "$CITY" | sed '$ d' | sed '1,3d' | tr -d '|' | awk '{print $3,$4}' | sort -t' ' -k2 -n)
+        
+        SERVER=$(echo $SCRAPED_VALUES | awk '{print $1}')
+
+        echo "Try to connect to the best server ($SERVER)"
+
+        sudo cyberghostvpn --traffic --country-code $COUNTRY_CODE --city "$CITY" --server $SERVER --connect
+    else
+        echo "Error, no City avaiable for the country code selected"
+        exit 1
+    fi
 fi
-
-echo "Try to connect to the best server ($SERVER)"
-
-sudo cyberghostvpn --country-code $COUNTRY_CODE --city $CITY --server $SERVER --connect
